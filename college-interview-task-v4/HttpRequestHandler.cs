70f9ce95e5e790b5860c85722388a9382b13faf0
rozyckijan <rozyckijan@users.noreply.github.com>
2020-09-29 14:07:20 +0200

    First commit

---
+++ college-interview-task-v4/HttpRequestHandler.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Generic;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace college_interview_task_v4
+{
+    public abstract class HttpRequestHandler<TRequest, TResponse>
+    {
+        internal HttpClient _httpClientProxy;
+
+        private IHttpResponseParser<TResponse> _parser { get; }
+
+        protected HttpRequestHandler(HttpClient httpClientProxy, IHttpResponseParser<TResponse> parser)
+        {
+            _parser = parser;
+        }
+
+        public async Task<TResponse> Handle(TRequest request, object payload, HttpMethod methodName,
+            string pleaseProvideFullUrlHere,
+            IDictionary<string, string> additionalHeaders, CancellationToken cancellationToken)
+        {
+            var uriString = $"{_httpClientProxy.BaseAddress}{pleaseProvideFullUrlHere}";
+
+            var httpRequestMessage = new HttpRequestMessage(methodName, new Uri(uriString));
+
+            if (payload != null)
+            {
+                httpRequestMessage.Content = payload as HttpContent;
+            }
+
+            var headers = additionalHeaders;
+
+            if (headers != null)
+            {
+                foreach (var header in headers)
+                {
+                    httpRequestMessage.Headers.Add(header.Key, header.Value);
+                }
+            }
+
+            HttpResponseMessage response = await _httpClientProxy.SendAsync(httpRequestMessage, cancellationToken);
+
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new EndOfStreamException(string.Join(",", response.Headers));
+            }
+
+            try
+            {
+                return _parser.ParseAsync(response);
+            }
+            catch (Exception ex)
+            {
+                throw new NotImplementedException("todo: add missing implementation");
+            }
+        }
+
+        public interface IHttpResponseParser<out TResult>
+        {
+            TResult ParseAsync(HttpResponseMessage response);
+        }
+    }
+}
